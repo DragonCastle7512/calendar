349a5ea738b0e38f030f7d87aa80e7a56a587738
DragonCastle7512 <kimys75128386@gmail.com>
2026-04-28 16:27:26 +0900

    fix: 위젯 사용 과정에서 발생하는 Warn 오류 방지
    - No task registered for key RNWidgetBackgroundTask 오류 방지(registerWidgetTaskHandler 별도의 index.js에 분리)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,5 @@
 import { Slot } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { registerWidgetTaskHandler } from 'react-native-android-widget';
-import { widgetTaskHandler } from '../src/widgets/widget-task-handler';
-
-registerWidgetTaskHandler(widgetTaskHandler);
 
 export default function RootLayout() {
   return (

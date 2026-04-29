bc4179d3737b129861e279fc1e509e13a2c76557
DragonCastle7512 <kimys75128386@gmail.com>
2026-04-30 03:21:28 +0900

    fix: 아이콘 svg로 대체
    - 네이티브 파일 인식 실패로, svg 아이콘으로 대체 및 기존 xml 아이콘 제거

--- src/widgets/MemoWidget.tsx
+++ src/widgets/MemoWidget.tsx
@@ -1,8 +1,11 @@
 "use no memo";
 
 import React from 'react';
-import { FlexWidget, IconWidget, TextWidget } from 'react-native-android-widget';
+import { FlexWidget, SvgWidget, TextWidget } from 'react-native-android-widget';
+
 import { MemosState } from '../types/calendar';
+
+const SETTINGS_ICON_SVG = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#8A8A8A"><path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z"/></svg>`;
 
 interface MemoWidgetProps {
   year: number;
@@ -17,10 +20,10 @@ interface MemoWidgetProps {
   alignment?: 'top' | 'center';
 }
 
-export function MemoWidget({ 
-  year, 
-  month, 
-  days, 
+export function MemoWidget({
+  year,
+  month,
+  days,
   memos = {},
   todayStr = '',
   holidays = {},
@@ -30,7 +33,7 @@ export function MemoWidget({
   alignment = 'top'
 }: MemoWidgetProps) {
   const weekDays = ['일', '월', '화', '수', '목', '금', '토'];
-  const alpha = '4D'; 
+  const alpha = '4D';
 
   // Font size scaling
   const scales = [0.85, 1, 1.15];
@@ -45,17 +48,20 @@ export function MemoWidget({
         flexDirection: 'column',
       }}
     >
-      <FlexWidget style={{ 
-        flexDirection: 'row', 
+      <FlexWidget style={{
+        flexDirection: 'row',
         width: 'match_parent',
-        alignItems: 'center', 
-        justifyContent: 'space-between', 
+        alignItems: 'center',
+        justifyContent: 'space-between',
         paddingVertical: 8,
-        borderBottomWidth: 1, 
-        borderBottomColor: '#F0F0F0' 
+        borderBottomWidth: 1,
+        borderBottomColor: '#F0F0F0'
       }}>
-        <FlexWidget clickAction="OPEN_SETTINGS_APP" style={{ marginLeft: 10, padding: 6, backgroundColor: '#F0F4F8', borderRadius: 20 }}>
-          <IconWidget font='material' icon="ic_settings" size={20} />
+        <FlexWidget clickAction="OPEN_SETTINGS_APP" style={{ marginLeft: 10, padding: 6, borderRadius: 20 }}>
+          <SvgWidget
+            svg={SETTINGS_ICON_SVG}
+            style={{ height: 22, width: 22 }}
+          />
         </FlexWidget>
         <FlexWidget style={{ flexDirection: 'row', alignItems: 'center' }}>
           <FlexWidget clickAction="PREV_MONTH" style={{ paddingHorizontal: 15, paddingVertical: 5 }}>
